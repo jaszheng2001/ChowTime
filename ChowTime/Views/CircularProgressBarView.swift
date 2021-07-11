aac7b7279ed5f9775401525e7ffbf43dddbbe2ac
Jason Zheng <jaszheng2001@gmail.com>
2021-07-10 20:12:47 -0400

    add nutrition in planner

---
+++ ChowTime/Views/CircularProgressBarView.swift
@@ -0,0 +1,20 @@
+//
+//  CircularProgressBarView.swift
+//  ChowTime
+//
+//  Created by Jason Zheng on 7/10/21.
+//
+
+import UIKit
+
+class CircularProgressBarView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ChowTime/Views/PlannerViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlannerViewController.swift
+//  ChowTime
+//
+//  Created by Jason Zheng on 7/10/21.
+//
+
+import UIKit
+
+class PlannerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
